b91e552298449dc3e8e63f81496eee4646019915
gussv375 <gussv375@tlhw-5-4.ad.liu.se>
2020-04-15 10:33:59 +0200

    Added lab6 and lab5 code skeleton

---
+++ Lab5/src/GC/header.py
@@ -0,0 +1,26 @@
+def header_get_garbage_flag(heap, pointer):
+  pass
+
+def header_set_garbage_flag(heap, pointer, value):
+  pass
+
+def header_get_used_flag(heap, pointer):
+  pass
+  
+def header_set_used_flag(heap, pointer, value):
+  pass
+
+def header_is_pointers_array(heap, pointer):
+  pass
+  
+def header_mark_as_pointers_array(heap, pointer):
+  pass
+
+def header_mark_as_bytes_array(heap, pointer):
+  pass
+  
+def header_get_size(heap, pointer):
+  pass
+
+def header_set_size(heap, pointer, size):
+  pass
