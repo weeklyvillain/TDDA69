deccc3d88bf65b96ee447c1f2b179a6bc5a57bca
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2020-06-04 18:24:44 +0200

    Lab6 done

--- Lab6/src/SQpy/database.py
+++ Lab6/src/SQpy/database.py
@@ -63,9 +63,11 @@ class database(object):
 
       # If it is a list, check if there is any operations in the select statement.
       # If there is a select statement, get the operand values
+      print(query.columns)
       if type(query.columns) is list:
         for idx, column in enumerate(query.columns):
           if type(column) is tuple:
+            print(column[1])
             query.columns[idx] = (self.get_operand_values(column[0]), column[1])
 
       where = None
@@ -74,36 +76,23 @@ class database(object):
 
       joinedColumns = []
       listOfJoins = []
+
+      # If we should join
       if hasattr(query, "joins"):
         newTable = table_obj
+        i = 0
+        # Join all the tables from the query.join
         for join in query.joins:
           join_table = self.get_table_object(join.table)
-          newTable = newTable.join((join_table, self.get_operand_values(join.on)))
-        table_obj = newTable
-
-        print(query.columns)
-        i = 0
-        # Gör om query.columns så ifall det är ['TABLE', 'COLUMN'] så ska det bli 'TABLE_COLUMN'
-        for column in query.columns:
-          if isinstance(column[0], list):
-            lst = list(column)
-            lst[0] = lst[0][0] + "_" + lst[0][1]
-            query.columns[i] = tuple(lst)
-          if isinstance(column[0], Query):
-
-            query.columns[i] = column[0]._replace(lh=column[0].lh[0] + "_" + column[0].lh[1])
-            query.columns[i] = column[0]._replace(rh=column[0].rh[0] + "_" + column[0].rh[1])
-            #column[0].lh = column[0].lh[0] + "_" + column[0].lh[1]
-            #column[0].rh = column[0].rh[0] + "_" + column[0].rh[1]
+          newTable = newTable.join((join_table, self.get_operand_values(join.on)), i == 0)
           i += 1
-        print(query.columns)
+        table_obj = newTable
       
       return table_obj.select(query.columns, where)
 
 
 
   def get_operand_values(self, operand):
-
     if operand.token == token.op_and:
       to_return = []
       for op in operand.operands:
@@ -113,6 +102,9 @@ class database(object):
       return Query(operand.token, operand.field, None)
     
     elif operand.token == token.identifier:
+      print(operand.identifier)
+      if len(operand.identifier) > 1:
+        return ['_'.join(operand.identifier)]
       return operand.identifier
 
     elif operand.token == token.op_divide or operand.token == token.op_equal:
@@ -150,110 +142,54 @@ class Table():
     self.content.append(self.definition(*values))
 
   def insert_with_columns(self, values, columns):
-    #row = self.definition("a=hej")
     to_be_inserted = dict(zip(columns, values))
     self.content.append(self.definition(**to_be_inserted))
 
-  def join(self, table):
+  def join(self, table, shouldSplit=True):
     query = table[1]
     joinedTables = []
-    print("TABLES")
-    print(table)
     fields = []
+    # Make the new fields for the new joined table
+    # This is for the other table (not self)
     for field in table[0].definition._fields:
       fields.append(table[0].name + "_" + field)
+    # This is for this (self) table, if we shouldn't split (more than one join), we only append the field so we don't get multiple self.name in the field names.
     for field in self.definition._fields:
-      fields.append(self.name + "_" + field)
+      if shouldSplit:
+        fields.append(self.name + "_" + field)
+      else:
+        fields.append(field)
 
+
+    # Create the new table
     newTable = Table("joinedTable", fields)
-    print(newTable.definition._fields)
 
-    print(self.content)
-
+    # Go over all the rows in this (self) table and in the other table that we are joining with
     for row in self.content:
       for row2 in table[0].content:
+        # If we should perform an equal
         if query.token == token.op_equal:
-          if query.lh[0] == self.name:
-            if getattr(row, query.lh[1]) == getattr(row2, query.rh[1]):
-              newTable.insert([*(row + row2)])
+          # If we should split the table names (if we are running more then one join), split the names on the delimiter '_'
+          if shouldSplit:
+            lhColumns = query.lh[0].split('_')
+            rhColumns = query.rh[0].split('_')
           else:
-            if getattr(row, query.rh[1]) == getattr(row2, query.lh[1]):
-              newTable.insert([*(row + row2)])
-
-    print("CONTENT")
-    print(newTable.content)
+            lhColumns = [query.lh[0].split('_')[0]] + query.lh
+            rhColumns = [''] + query.rh
+          # Needed to know if the table name is at the left or at the right of the lhColumns array
+          if lhColumns[0] == self.name:
+            # Split the rh query since row2 (which rhColumns is only used on) is not joined on before, and won't include any '_'
+            rhColumns = query.rh[0].split('_')
+            # If the two attributes are equal, insert them to the new table
+            if getattr(row, lhColumns[1]) == getattr(row2, rhColumns[1]):
+              newTable.insert([*(row2 + row)])
+          else:
+            # Split the lh query since row2 (which lhColumns is only used on) is not joined on before, and won't include any '_'
+            lhColumns = query.lh[0].split('_')
+            # If the two attributes are equal, insert them to the new table
+            if getattr(row, rhColumns[1]) == getattr(row2, lhColumns[1]):
+              newTable.insert([*(row2 + row)])
     return newTable
-
-
-
-    """
-    
-
-
-
-    """
-    return None
-
-
-
-    """
-
-    # Create new row definition
-    columns = []
-    for table in tables:
-      tableObj = table[0]
-      columns = columns + tableObj.columns
-
-    joinedRowDefinition = namedtuple(self.name, columns, defaults=(None,) * len(columns))
-    print(columns)
-
-
-    tableContents = [] # (tableObj/name, table content (allt)) OBS: INKLUSIVE KOLUMN NAMN OCH VÄRDE
-    for table in tables:
-      tableObj = table[0]
-      rows = []
-      for row in tableObj.content:
-        rows.append(row)
-      tableContents.append((tableObj, rows))
-      # Hämta alla rader från tableObj och lägg in i tableContents som tuple
-    print("TABLEEEEEEEE")
-    print(tableContents)
-
-    for c in tableContents:
-      row = {}
-      # Find the correct Table query
-      for joinTable in tables:
-        if joinTable[0].name == c[0].name:
-          for tableRow in c[1]:
-            query = joinTable[1]
-            print("query")
-            print(query)
-            if query.token == token.op_equal:
-              for selfRow in self.content:
-                if query.lh[0] == self.name:
-                  if getattr(selfRow, query.lh[1]) == getattr(tableRow, query.rh[1]):
-                    #test = namedtuple("test", joinTable[0].definition._fields + self.definition._fields)
-                    fields = []
-                    for field in joinTable[0].definition._fields:
-                      fields.append(joinTable[0].name+ field)
-                    for field in self.definition._fields:
-                      fields.append(self.name+ field)
-                    test = namedtuple("test", fields, defaults=(None,) * len(fields))
-                    print(tableRow)
-                    print(selfRow)
-                    l = test(*(tableRow + selfRow))
-                    print("HAHAHAH")
-                    print(l)
-                else:
-                  if getattr(selfRow, query.rh[1]) == getattr(tableRow, query.lh[1]):
-                    print("HAHAHHOOHOHOAH")
-            pass
-            # Do the ON shit here with the table that corresponds to the current tableContent
-            # if true:
-            # row[tableContents[1][KOLUMNENS NAMN PÅ NÅGOT VÄNSTER]] = tableContents[1][KOLUMNENS VÄRDE PÅ NÅGOT VÄNSTER]
-            # joinedTables.append(row)
-          # joinedTables.append(row)
-    """
 
 
   def select(self, columns, where=None):
@@ -290,16 +226,18 @@ class Table():
           # If the column if a tuple, we need to do the calculation accorind to the operand
           # and save it with the name of the AS value (column[1])
           if type(column) is tuple:
-            print("TYP")
-            print(type(column[0]))
             # If it is AS without a Query object (without a token)
             if isinstance(column[0], list):
-              print("kolla")
-              print(column[0][0])
-              d[column[0][1]] = getattr(row, column[0][1])
+              d[column[1]] = getattr(row, column[0][0])
 
-            if column[0].token == token.op_divide:
-              d[column[1]] = getattr(row, column[0].lh[0]) / column[0].rh
+            elif isinstance(column[0], str):
+              d[column[1]] = getattr(row, column[0])
+
+            elif column[0].token == token.op_divide:
+              if isinstance(column[0].rh, list):
+                d[column[1]] = getattr(row, column[0].lh[0]) / getattr(row, column[0].rh[0])
+              else:
+                d[column[1]] = getattr(row, column[0].lh[0]) / column[0].rh
               # If it is a count expression
             elif column[0].token == token.fn_count:
               should_append = False
@@ -369,7 +307,6 @@ class Table():
 
           if matched_condition:
             if idx in found_columns:
-              print("PLUS")
               found_columns[idx]["count"] += 1
             else:
               found_columns[idx] = {"count": 1}
@@ -411,7 +348,6 @@ class Table():
 
           if matched_condition:
             if idx in found_columns:
-              print("PLUS")
               found_columns[idx]["count"] += 1
             else:
               found_columns[idx] = {"count": 1}
